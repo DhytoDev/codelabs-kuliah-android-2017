6fb751540a24d3c08258f766ca46bb6884818608
Fordyta Abubakar <dhytodev@gmail.com>
2017-12-01 07:01:26 +0800

    1-ActivityLifeCycle / override semua method activity lifecycle kemudian cetak setiap perubahan state ke textview mLifecycleStatus dan logcat

--- 1-ActivityLifeCycle/app/src/main/java/com/dhytodev/activitylifecycle/MainActivity.java
+++ 1-ActivityLifeCycle/app/src/main/java/com/dhytodev/activitylifecycle/MainActivity.java
@@ -11,12 +11,56 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mLifecycleStatus ;
 
+    private static final String ON_CREATE = "onCreate";
+    private static final String ON_START = "onStart";
+    private static final String ON_RESUME = "onResume";
+    private static final String ON_PAUSE = "onPause";
+    private static final String ON_STOP = "onStop";
+    private static final String ON_RESTART = "onRestart";
+    private static final String ON_DESTROY = "onDestroy";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mLifecycleStatus = (TextView) findViewById(R.id.tv_lifecycle_events_display);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        logAndAppend(ON_START);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        logAndAppend(ON_RESUME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        logAndAppend(ON_PAUSE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        logAndAppend(ON_STOP);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        logAndAppend(ON_RESTART);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        logAndAppend(ON_DESTROY);
     }
 
     private void logAndAppend(String lifecycleStatus) {
