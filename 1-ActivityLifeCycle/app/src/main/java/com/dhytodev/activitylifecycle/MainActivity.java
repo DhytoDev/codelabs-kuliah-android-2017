72bbe966dfdceb3cdf2464c19e63859048ba0538
Fordyta Abubakar <dhytodev@gmail.com>
2017-12-01 06:52:03 +0800

    1-ActivityLifeCycle / menambah method “logAndAppend(String lifecycleStatus)” yang berfungsi untuk mencetak setiap perubahan state lifecycle

--- 1-ActivityLifeCycle/app/src/main/java/com/dhytodev/activitylifecycle/MainActivity.java
+++ 1-ActivityLifeCycle/app/src/main/java/com/dhytodev/activitylifecycle/MainActivity.java
@@ -2,12 +2,25 @@ package com.dhytodev.activitylifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView mLifecycleStatus ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLifecycleStatus = (TextView) findViewById(R.id.tv_lifecycle_events_display);
+    }
+
+    private void logAndAppend(String lifecycleStatus) {
+        Log.d(TAG, "Lifecycle Event: " + lifecycleStatus);
+        mLifecycleStatus.append(lifecycleStatus + "\n");
     }
 }
